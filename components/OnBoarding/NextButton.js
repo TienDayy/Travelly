74eda0c993495c5c11d05ec20936483abd00bfd6
Tran Tien <thanhtienst2004@gmail.com>
2024-06-29 00:12:25 +0700

    Fix bug successfully

--- components/OnBoarding/NextButton.js
+++ components/OnBoarding/NextButton.js
@@ -4,8 +4,8 @@ import { Svg, Rect, G } from "react-native-svg";
 import Slides from './slides';
 import FontLoader from "../FontLoader";
 
-const width = 344;
-const height = 73;
+const width = 342;
+const height = 76;
 const strokeWidth = 2;
 const borderRadius = 25;
 const viewBoxWidth = width + strokeWidth * 2;
@@ -15,29 +15,28 @@ const circumference = (width + height) * 2;
 export default function NextButton({ scrollTo, percentage, currentIndex }) {
     const progressAnimation = useRef(new Animated.Value(0)).current;
     const progressRef = useRef(null);
+
     const animation = (toValue) => {
-        //console.log('DeBug1');
         return Animated.timing(progressAnimation, {
             toValue,
             duration: 250,
-            useNativeDriver: true,
+            useNativeDriver: false,  // Ensure this is set to false
         }).start();
     };
 
     useEffect(() => {
         animation(percentage);
     }, [percentage]);
-   
-    useEffect(() => { 
+
+    useEffect(() => {
         progressAnimation.addListener((value) => {
-            //console.log('Debug2');
             const strokeDashoffset = circumference - (circumference * value.value) / 100;
             if (progressRef?.current) {
                 progressRef.current.setNativeProps({
                     strokeDashoffset,
                 });
             }
-        }, [percentage]);
+        });
 
         return () => {
             progressAnimation.removeAllListeners();
@@ -46,40 +45,40 @@ export default function NextButton({ scrollTo, percentage, currentIndex }) {
 
     return (
         <FontLoader>
-        <View style={styles.container}>
-            <Svg width={viewBoxWidth} height={viewBoxHeight} viewBox={`0 0 ${viewBoxWidth} ${viewBoxHeight}`}>
-                <G rotation="0" origin={width}>
-                <Rect
-                        x={strokeWidth / 2}
-                        y={strokeWidth / 2}
-                        width={width}
-                        height={height}
-                        rx={borderRadius}
-                        ry={borderRadius}
-                        stroke="#E6E7E8"
-                        strokeWidth={strokeWidth}
-                        fill="transparent"
-                    />
-                    <Rect
-                        ref={progressRef}
-                        x={strokeWidth / 2}
-                        y={strokeWidth / 2}
-                        width={width}
-                        height={height}
-                        rx={borderRadius}
-                        ry={borderRadius}
-                        stroke="#FF9933"
-                        strokeWidth={strokeWidth}
-                        fill="transparent"
-                        strokeDasharray={circumference}
-                        strokeDashoffset = {circumference}
-                    />
-                </G>
-            </Svg>
-            <TouchableOpacity activeOpacity={1} style={styles.nextButton} onPress={scrollTo}>
+            <View style={styles.container}>
+                <Svg width={viewBoxWidth} height={viewBoxHeight} viewBox={`0 0 ${viewBoxWidth} ${viewBoxHeight}`}>
+                    <G rotation="0" origin={width}>
+                        <Rect
+                            x={strokeWidth / 2}
+                            y={strokeWidth / 2}
+                            width={width}
+                            height={height}
+                            rx={borderRadius}
+                            ry={borderRadius}
+                            stroke="#E6E7E8"
+                            strokeWidth={strokeWidth}
+                            fill="transparent"
+                        />
+                        <Rect
+                            ref={progressRef}
+                            x={strokeWidth / 2}
+                            y={strokeWidth / 2}
+                            width={width}
+                            height={height}
+                            rx={borderRadius}
+                            ry={borderRadius}
+                            stroke="#FF9933"
+                            strokeWidth={strokeWidth}
+                            fill="transparent"
+                            strokeDasharray={circumference}
+                            strokeDashoffset={circumference}
+                        />
+                    </G>
+                </Svg>
+                <TouchableOpacity activeOpacity={1} style={styles.nextButton} onPress={scrollTo}>
                     <Text style={styles.nextButtonText}>{currentIndex === Slides.length - 1 ? "Let's start!" : "Next"}</Text>
-            </TouchableOpacity>
-        </View>
+                </TouchableOpacity>
+            </View>
         </FontLoader>
     );
 }
@@ -93,14 +92,13 @@ const styles = StyleSheet.create({
     },
     nextButton: {
         position: 'absolute',
-        width: 333,
+        width: 329,
         height: 61,
-        bottom: 61,
-        left: (width - 332) / 2,
-        paddingVertical: 18,
-        paddingHorizontal: 40,
-        borderRadius: 20,
+        padding: 18,
+        borderRadius: 18,
         backgroundColor: '#FEA36B',
+        justifyContent: 'center',
+        alignItems: 'center',
     },
     nextButtonText: {
         color: '#fff',
